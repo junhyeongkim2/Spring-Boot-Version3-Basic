2837752bfffe1240ffe36613ab0d5c7ffa138372
jun <ggprgrkjh@naver.com>
2023-07-01 18:51:18 +0900

    feat: junitFailedTest

--- src/test/java/study/third/JUnitTest.java
+++ src/test/java/study/third/JUnitTest.java
@@ -11,7 +11,17 @@ public class JUnitTest {
         int a = 1;
         int b = 2;
         int sum = 3;
-
         Assertions.assertEquals(a+b,sum);
     }
+
+    @DisplayName("1 + 3은 4이다")
+    @Test
+    public void junitFailedTest(){
+        int a = 1;
+        int b = 3;
+        int sum = 3;
+        Assertions.assertEquals(a+b,sum);
+    }
+
+
 }
